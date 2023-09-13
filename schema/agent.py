0a6ff5b88bf4a569dc751751c073cc2521a6d66d
payman hodaie <payman@osellus-mobile.com>
2023-09-13 12:55:53 -0400

    minor changes

--- schema/agent.py
+++ schema/agent.py
@@ -4,4 +4,4 @@ from typing import Optional
 class Agent(BaseModel):
     title : str = Field(...,description="Agent's display title")
     role : str = Field(...,description="Agent's role")
-    first_message : Optional[str]
+    first_message : Optional[str] = Field(default=None)
